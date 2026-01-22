4f6058e2bf917f32845a8fa29c0f58f87936938d
Jali-dev <jalidev7@gmail.com>
2026-01-21 22:53:15 -0500

    Update production URL to Render

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 // Core constants will go here
 class AppConstants {
   static const String appName = 'Fluxo';
-  // Production Backend URL (Koyeb)
-  static const String kBackendUrl = 'https://delightful-flori-jalids-e6d15453.koyeb.app'; 
+  // Production Backend URL (Render)
+  static const String kBackendUrl = 'https://fluxo-backend-w7xs.onrender.com'; 
 }
